34a0aad53e9a5ce750cbf4c921d6f20f258a672f
Karen Spriggs <kspriggs@colum.edu>
2020-11-17 11:49:08 -0600

    .

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -11,15 +11,30 @@
 using namespace B;
 
 int round = 0;
-Player* dealer = new Player();
-Player* player = new Player();
+Player dealer;
+Player player;
 Deck* deck = new Deck();
+
+void deal(){
+    int player_first;
+    int player_second;
+    int dealer_first;
+    int dealer_second;
+
+    player.update_total(player_first);
+    player.update_total(player_second);
+    dealer.update_total(dealer_first);
+    dealer.update_total(dealer_second);
+}
 
 void play() {
     // This is what is run 100 times
     // First you deal the first 2 cards to the players, player goes first then dealer
     // They choose to either stand or hit depending on what they have
     // Whoever wins gets a point
+    deal();
+    int player_choice = player.make_choice();
+    int dealer_choice = player.make_choice();
 
     round++;
 }
--- Blackjack/Player.h
+++ Blackjack/Player.h
@@ -1,5 +1,11 @@
 namespace B {
     class Player{
-        
+        public:
+            int score = 0;
+            Player() {
+
+            }
+            void update_total(int value);
+            int make_choice();
     };
 }
