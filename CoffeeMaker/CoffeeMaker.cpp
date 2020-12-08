bada0e997b71b4cdfa6369c7c6ce3a0d7a50778d
Karen Spriggs <kspriggs@colum.edu>
2020-12-08 17:27:45 -0600

    Update CoffeeMaker.cpp

--- CoffeeMaker/CoffeeMaker.cpp
+++ CoffeeMaker/CoffeeMaker.cpp
@@ -9,32 +9,32 @@
 using namespace C;
 using namespace std;
 
+int cups = 0;
 Hopper* hopper = new Hopper();
 Reservoir* reservoir = new Reservoir();
 
-int take_input(){
-    int cups = 0;
-    std::cout << "Please enter the amount of cups of coffee you want to make" << std::endl;
+void take_input() {
+    //int cups;
+    std::cout << "Please enter the amount of cups of coffee you want to make, between 1 and 4" << std::endl;
     // Make sure that you have a try/catch for exceptions here
     std::cin >> cups;
 
-    if (cups > 4){
+    if ((cups > 4) || (cups == 0)){
+        std::cout << "Please enter an amount between 1 and 4" << std::endl;
         take_input();
-    } else {
-        return cups;
     }
 }
 
-void make_coffee(int cups){
+void make_coffee(int cups) {
     std::cout << "Making coffee: " << cups << " cups" << std::endl;
 }
 
-void finish(int cups){
+void finish(int cups) {
     std::cout << "done" << std::endl;
 }
 
-void make(){
-    int cups = take_input();
+void make() {
+    take_input();
 
     hopper->fill(cups);
     hopper->grind();
@@ -44,6 +44,6 @@ void make(){
     finish(cups);
 }
 
-int main(){
+int main() {
     make();
 }
